d5dc8350cf19bc1f2d9e25f6ec22553d92a5cb4e
Dan Pocock <danjpocock@gmail.com>
2023-11-24 19:27:57 -0700

    Revision 1

--- app.py
+++ app.py
@@ -3,45 +3,12 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import numpy as np
-import calendar
 import plotly.graph_objects as go
 
 # Read and clean data
 # Read file
-df = pd.read_csv('ufo-sightings-transformed.csv', parse_dates=['Date_time'])
-
-# Drop unnamed column
-df = df.iloc[:, 1:]
-
-# Make columns lowercase for easier referencing
-df.columns = df.columns.str.lower()
-
-# Change name of confusing duration column name
-df = df.rename(columns={'length_of_encounter_seconds': 'duration_secs'})
-
-# Change month number to month name
-df['month'] = df['month'].apply(lambda x: calendar.month_name[x])
-
-# Create duration in minutes column
-df['duration_mins'] = df['duration_secs'] / 60
-
-# Create duration in hours
-df['duration_hours'] = df['duration_mins'] / 60
-
-# Create duration in days
-df['duration_days'] = df['duration_hours'] / 24
-
-# Create age column as 2023 minus the year and month
-df['age'] = 2023 - (df['date_time'].dt.year + (df['date_time'].dt.month/12))
-
-# Replace '&#44' with ' in description column
-df['description'] = df['description'].str.replace('&#44', "")
-
-# Replace '&#39' with ' in description column
-df['description'] = df['description'].str.replace('&#39', "")
-
-# Replace '&#33' with '.' in description column
-df['description'] = df['description'].str.replace('&#33', '.')
+df = pd.read_csv('ufo-sightings-cleaned.csv', parse_dates=
+                 ['encounter_date','reported_diff'], infer_datetime_format=True)
 
 # Create a text header above the dataframe
 st.header('UFO Sightings')
@@ -56,116 +23,199 @@ The data contains information about UFO encounters from 1906 to 2014. Details ab
 st.table(df.drop(columns='description').head(10))
 
 # Create histogram header
-st.header('Duration Analysis')
+st.header('Frequency Analysis')
 
 st.write("""
-###### Does the duration of the encounter change depending on the shape of the observed ufo? The country and state it is observed in? How about the month or season?
+###### How frequently are sightings occurring? How does this change over time? 
 """)
 
+# Find min and max encounter years
+(min_year,max_year) = (df['encounter_date'].dt.year.min(),df['encounter_date'].dt.year.max())
+
+# Create slider for encounter years
+year_range = st.slider('Select a range of years:', min_year, max_year, (min_year, max_year))
+
 # Create selection box for duration length
-duration_for_hist = st.selectbox('Duration length by first:',  [
-                                 'minute', 'hour', 'day'])
+legend_variable = st.selectbox('Filter by:',[
+                                 'Season', 'Continent','Time of Day'], 
+                               key=1)
 
-# Save list of countries as countries
-countries = df['country'].unique()
+# Create new dataframe with filtered years
+filtered_df = df[(df['encounter_date'].dt.year >= year_range[0]) & (
+    df['encounter_date'].dt.year <= year_range[1])]
 
-# Create select box to filter by country
-name_country = st.selectbox('Select country:', countries)
+# Save legend_variable to reference appropriate legend columns in histogram
+d={}
+d['Season'] = 'season'
+d['Continent'] = 'continent'
+d['Time of Day'] = 'time_of_day'
 
-# Dynamically create variables to provide changes to x-axis dependent on the duration selectbox
-d = {}
-d['minute'] = 'duration_secs'
-d['hour'] = 'duration_mins'
-d['day'] = 'duration_hours'
-d['millenia'] = 'duration_days'
 
-# Dynamically create variables to provide changes to x-axis dependent on the duration selectbox
-e = {}
-e['minute'] = 'duration_mins'
-e['hour'] = 'duration_hours'
-e['day'] = 'duration_days'
+# Save legend_variable to reference appropriate category orders in histogram
+e={}
+e['Season'] = ['Spring', 'Summer', 'Autumn', 'Winter']
+e['Continent'] = ['North America', 'South America', 'Europe', 'Asia', 'Africa', 'Oceania']
+e['Time of Day'] = ['Morning', 'Afternoon', 'Evening', 'Night']
 
-# Create filtered df by combining country and duration length
-filtered_df = df[(df['country'] == name_country) &
-                 (df[e[duration_for_hist]] < 1)]
+# Create histogram of ufo sightings duration per continent
+fig = px.histogram(filtered_df, 
+                   x='encounter_year', 
+                   color=d[legend_variable], 
+                   title='UFO Sightings Duration per Continent', 
+                   labels={'duration_secs':'Duration (minutes)', 'continent':'Continent'},
+                   category_orders={d[legend_variable]:e[legend_variable]})
 
-# Create list for histogram variables
-var_list_for_hist = ['region', 'ufo_shape', 'season', 'month']
-
-# Create selection box for region, shape, season and month
-var_for_hist = st.selectbox(
-    'Split for duration distribution:', var_list_for_hist)
-
-# Order months for legend
-months_ordered = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-                  'September', 'October', 'November', 'December']
-
-# Create histogram
-fig = px.histogram(filtered_df, x=d[duration_for_hist], color=var_for_hist, category_orders={
-                   'month': months_ordered}, nbins=24)
-
-# Rename axis, title and legend
+# Update layout
 fig.update_layout(
-    title='Split of encounter duration by {}</b>'.format(var_for_hist))
+    title={
+        'text': f"UFO Sightings Count and Duration per {legend_variable}",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    xaxis_title="Duration (minutes)",
+    yaxis_title="Number of Sightings",
+    legend_title="Continent",
+    font=dict(
+        family="Courier New, monospace",
+        size=12,
+        color="RebeccaPurple"
+    )
+)
 
-# Display the Chart
 st.plotly_chart(fig)
 
-# Create scatterplot header
-st.header('Age Analysis')
+st.write("Encounters are increasing at a dramatic rate. More encounters are now occurring in Summer when Autumn used to have more."
+)
+#----------------------------------------------------------------------------------------------------------------------------
 
-st.write("""
-###### How does the age of a reported case affect duration in terms of country, season and month?
-""")
+st.header('Report Date Lag')
 
-# Create dataframes for different duration lengths by dynamically setting variables
-f = {}
-f['minute'] = df[df['duration_mins'] <= 1]
-f['hour'] = df[df['duration_hours'] <= 1]
-f['day'] = df[df['duration_days'] <= 1]
-f['millenia'] = df
+# write string in streamlit
+st.write("""Validity may be tested by comparing the relationship of the duration 
+         of an encounter with the encounter-report age lag (the difference between 
+         the encounter date and the date it was reported). In short, this answers 
+         whether the details of the story might change over time. Where there is a 
+         large encounter-report date gap, perhaps the duration is being reported 
+         longer than those with a smaller gap.
+         """)
 
-# Create selection box for duration length
-duration_for_scatter = st.selectbox('Duration length by first:',  [
-                                    'minute', 'hour', 'day', 'millenia'], key=1)
+# Create slider for encounter years
+year_range2 = st.slider('Select a range of years:', min_year, max_year, (min_year, max_year), key=2)
 
-list_for_scatter = ['country', 'season', 'month']
-choice_for_scatter = st.selectbox('Filter by:', list_for_scatter)
+# Create selection box for report date lag
+legend_variable2 = st.selectbox('Filter by:',['Season', 'Continent','Time of Day'], key=3)
 
-fig2 = px.scatter(f[duration_for_scatter], x=d[duration_for_scatter], y='age', color=choice_for_scatter,
-                  hover_data='description', category_orders={'month': months_ordered})
+# Create new dataframe with filtered years
+filtered_df2 = df[(df['encounter_date'].dt.year >= year_range[0]) & (
+    df['encounter_date'].dt.year <= year_range[1])]
+
+# Filter dataframe to only include encounters with less than 1 hour duration (filter out outliers)
+filtered_df2 = filtered_df2[filtered_df2['duration_hours'] <= 1]
+
+# Create scatter plot with specific colors for each month
+fig2 = px.scatter(filtered_df2, 
+                 x='reported_diff', 
+                 y='duration_mins', 
+                 color=d[legend_variable], 
+                 category_orders={d[legend_variable]:e[legend_variable]},
+                 opacity=0.5
+                 )
+
+# Update titles
+fig2.update_layout(
+    title={
+        'text': "Report Date Lag and Encounter Duration",
+        'y':0.95,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    xaxis_title="Reported Difference (years)",
+    yaxis_title="Duration (mins)",
+    legend_title='Season',
+    font=dict(
+        family="Courier New, monospace",
+        size=14,
+        color="black")
+    
+)
 
 st.plotly_chart(fig2)
 
-# Create scatterplot header
+st.write("""
+If we expected a reported duration to increase over time the scatter plot would reflect this. 
+This doesn't seem to be 
+the case as every encounter with a report lag of sixty years plus 
+do not exceed those found in previous years.
+""")
+#----------------------------------------------------------------------------------------------------------------------------
+
+# Create mape header
 st.header('A Map of UFO Encounters')
 
 st.write("""
 ###### Here are the encounters presented on a map. UFO shapes can be selected to show where different shapes have been found around the world. Each encounter is colored based on the age it was last seen. When hovering, a description by the viewer is given.
 """)
 
-# Find unique shapes
+# Create radio
+radio = st.radio('Select a map:', ['World View', 'United States'])
+
+# Create new dataframe with only United States encounters
+united_states_df = df[df['country'] == 'United States']
+
+# Find unique shapes and colors
 shapes = df['ufo_shape'].unique()
+colors = df['ufo_color'].unique()
+
+# Add 'All' option to shapes and colors
+shapes = np.append(shapes, 'All').astype(str)
+colors = np.append(colors, 'All').astype(str)
+
+# Re-order shapes and colors using sorted
+shapes = sorted(shapes)
+colors = sorted(colors)
+
+#If united states is checked, filter df to only include united states
+if radio == 'United States':
+    united_states_df = df[df['country'] == 'United States'] 
 
 # Create shapes select box
 shapes_for_map = st.selectbox('Shape of the sighted UFO:', shapes)
 
+# Create color select box
+colors_for_map = st.selectbox('Color of the sighted UFO:', colors)
+
+# Allow user to select all shapes
+if shapes_for_map == 'All' and colors_for_map == 'All':
+    filtered_df3 = df
+    
+# Allow user to select all colors
+elif colors_for_map == 'All':
+    filtered_df3 = df[(df['ufo_shape'] == shapes_for_map)]
+    
+# Allow user to select all shapes
+elif shapes_for_map == 'All':
+    filtered_df3 = df[(df['ufo_color'] == colors_for_map)]
+
+# Create geographical scatterplot, add colorbar to show age, zoom in on united states
+if radio == 'United States':
+    scope='usa'
+else:
+    scope='world'
+    
 # Create color scale
 scl = [0, "rgb(150,0,90)"], [0.125, "rgb(0, 0, 200)"], [0.25, "rgb(0, 25, 255)"], \
     [0.375, "rgb(0, 152, 255)"], [0.5, "rgb(44, 255, 150)"], [0.625, "rgb(151, 255, 0)"], \
     [0.75, "rgb(255, 234, 0)"], [0.875, "rgb(255, 111, 0)"], [
     1, "rgb(255, 0, 0)"]
-
-# create new df according to ufo_shape
-df1 = df[df['ufo_shape'] == shapes_for_map]
-
-# Create geographical scatterplot, add colorbar to show age
+    
+# Create geographical scatterplot
 fig3 = go.Figure(data=go.Scattergeo(
-    lat=df1['latitude'],
-    lon=df1['longitude'],
-    text=df1['description'],
+    lat=filtered_df3['latitude'],
+    lon=filtered_df3['longitude'],
+    text=filtered_df3['description'],
     marker=dict(
-        color=df1['age'],
+        color=filtered_df3['age'],
         colorscale=scl,
         reversescale=True,
         opacity=0.7,
@@ -178,10 +228,9 @@ fig3 = go.Figure(data=go.Scattergeo(
     )
 ))
 
-# Change layout to make it easier to view, add a title
 fig3.update_layout(
     geo=dict(
-        scope='world',
+        scope=scope,
         showland=True,
         landcolor='rgb(212, 212, 212)',
         subunitcolor='rgb(255, 255, 255)',
@@ -192,12 +241,9 @@ fig3.update_layout(
         showcountries=True,
         resolution=110,
     ),
-    title='Encounters Where the UFO Shape is a {}'.format(shapes_for_map),
-    height=800,
-    width=1000
-
-
+    title=f'Encounters Where the UFO Shape is {shapes_for_map} and Color is {colors_for_map}',
 )
+
 st.plotly_chart(fig3)
 
 st.write("""
--- notebooks/constants.py
+++
@@ -1,3 +0,0 @@
-APIKEY= "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"
-
-"Find the goal, why the goal is being pursued and the needs to achieve the goal."
